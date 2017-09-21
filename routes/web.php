ca309605f78cf00b720b9aa1ed448e9f50bf6169
jwarmour <jwarmour@ncsu.edu>
2017-09-21 17:42:46 -0400

    posts stuff from learning laravel 5.4 from scratchj

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+Route::get('/', 'PostsController@index');
+Route::get('/posts/{post}', 'PostsController@show');
 Route::get('/tasks', 'TasksController@index');
 Route::get('/tasks/{task}', 'TasksController@show');
 Route::get('about', function () {
